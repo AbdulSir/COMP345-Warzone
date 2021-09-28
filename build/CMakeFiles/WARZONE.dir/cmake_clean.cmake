c02234dae91bb7085a192c5afd5eeabb3472085a
wilson <wilsonhuang00815@gmail.com>
2021-09-28 18:58:44 -0400

    add cmake build config for version control

---
+++ build/CMakeFiles/WARZONE.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WARZONE.dir/main.cpp.o"
+  "WARZONE"
+  "WARZONE.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WARZONE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+#include "main.h"
+
+int main() {
+	return 0;
+}
