d1f0b80258903ab23bd22bd9cd18639ad7e05948
AbdulSir <44450682+AbdulSir@users.noreply.github.com>
2021-10-03 23:46:14 -0400

    Updated MapDriver.cpp file

--- MapDriver.cpp
+++ MapDriver.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <sstream>
 #include "Map.h"
 
 /*
 MapDriver.cpp file > creates a map by reading a set of map files
 MapDriver.cpp file > create a map object for valid maps (reject invalid map files)
 MapDriver.cpp file > Should demonstrate that the Map class does the following:
-	• the map is a connected graph
-	• continents are connected subgraphs
-	• each country belongs to one and only one continent.
+	â€¢ the map is a connected graph
+	â€¢ continents are connected subgraphs
+	â€¢ each country belongs to one and only one continent.
 
 MapDriver.cpp file
-	• reads many different map files
-	• creates a Map object for valid files
+	â€¢ reads many different map files
+	â€¢ creates a Map object for valid files
 rejects the invalid ones
 */
 using namespace std;
@@ -20,10 +22,7 @@ using namespace std;
 int main()
 {
     cout << "Testing if we can read the file \n";
-    MapLoader map_loader("ameroki.txt");
-    cout << "\n TEST \n TEST \n"; //europe.txt
-    Map map1();
-    //cout << map_loader.getText_contents();
-    //MapLoader map_loader1("europe.txt");
+    MapLoader map_loader("europe.txt");
     return 0;
 }
+
