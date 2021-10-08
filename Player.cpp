755b3e1fb94783aa79de1b0de20e9bf8247dc752
bcobo341 <beatrice.cobo@icloud.com>
2021-10-08 00:33:44 -0400

    met the requirements of player part

--- Player.cpp
+++ Player.cpp
@@ -1,81 +1,137 @@
-//
-//  Player.cpp
-//  Comp 345 a1
-//
-//  Created by Beatrice Cobo on 2021-09-28.
-//
+/**
+ Part 2: Player.cpp
+ */
 
 #include "Player.h"
 #include <iostream>
+#include <ostream>
 #include <string>
+using namespace std;
 
-Order::Order(){
-    
+//Order dummy default constructor
+Order::Order (){
+    this->description="order 1";
+    cout << "Order default constructor was called" << endl;
+}
+
+//Territorry dummy copy constructor
+Territory::Territory (const Territory &t1){
+    this->territory_name = t1.territory_name;
+    this->continent_ref=t1.continent_ref;
+    this->army_nb=t1.army_nb;
+    cout << "Inside Territory copy constructor" << endl;
+}
+
+//Territorry dummy constructor
+Territory::Territory(string terr_name, int contin_ref, int num_of_armies){
+    this->territory_name=terr_name;
+    this->continent_ref=contin_ref;
+    this->army_nb=num_of_armies;
 }
 
 //Default constructor
 Player::Player(){
     this->hand = new Hand();
-    //this->territories;
-    //this->orders;
-    this->name = "";
+    this->territories={};
+    this->orders= new OrdersList();
+    this->name = "Anonymous";
     cout << "Player created inside default constructor" << endl;
 }
 
 //Constructor
-Player::Player (string name, Hand * hand, list <Territory*> territories){
+Player::Player (string name, Hand * hand, vector <Territory*> territories){
     this->name = name;
     this->hand = hand;
     this->territories = territories;
-    cout << "Player " << name << " created inside constructor" << endl;
+    this->orders= new OrdersList();
+    cout << "Player created inside constructor" << endl;
 }
 
 //Copy constructor
 Player::Player(const Player& p){
   cout << "Inside copy constructor of Player" << endl;
     this->name = p.name;
-    this->hand = p.hand;
-    this->territories = p.territories;
-    this->orders = p.orders;
+    this->hand = new Hand;
+    *hand= *p.hand;
+    this->orders = new OrdersList;
+    *orders= *p.orders;
+    vector<Territory*> temp_t(p.territories.size());
+    this->territories= temp_t;
+    for (int i=0; i<p.territories.size(); i++)
+        territories[i] = new Territory(*p.territories[i]);
 }
 
-list <Territory*> Player::toDefend(){
-    cout << "Inside toDefend of Player" << endl;
-    list<Territory*> t;
-    return t;
-}
-
-list <Territory*> Player::toAttack(){
-    cout << "Inside toAttack of Player" << endl;
-    list<Territory*> t;
-    cout << "sizeof "<< sizeof(t)<<endl;
-    return t;
-}
-
-list <Order*> Player::issueOrder(){
-    cout << "Inside issueOrder of Player" << endl;
-    Order * order = new Order();
-    this->orders.push_back(order);
-    delete order;
-    order = NULL;
-    return orders;
+//Destructor
+Player::~Player()
+{
+    delete orders;
+    delete hand;
+    for (int i=0; i<territories.size(); i++){
+        delete territories[i];
+    }
 }
 
 //assignment operator
 Player& Player::operator=(const Player& p){
-  cout << "Inside operator = of Player" << endl;
-  name = p.name;
-  return *this;
+    cout << "Inside operator = of Player" << endl;
+    this->name = p.name;
+    this->hand = new Hand();
+    *hand = *p.hand;
+    this->orders = new OrdersList();
+    *orders = *p.orders;
+    vector<Territory*> temp_t(p.territories.size());
+    this->territories= temp_t;
+    for (int i=0; i<p.territories.size(); i++)
+        territories[i] = new Territory(*p.territories[i]);
+    return *this;
 }
 
 //stream insertion operator
-ostream& operator<<(ostream& ostmr, const Player& p)
+ostream& operator<<(ostream& out, const Player& p)
 {
-    return ostmr << "Player " << p.name;
+    out << "Player " << p.name<<" details:"<<endl ;
+    out << "-----------------------"<<endl;
+    out<< "Hand: "<<p.hand<<endl;
+    out<< "List of orders: "<<p.orders<<endl;
+    out<< "List of territories: ";
+    string separator;
+    for (auto territory : p.territories) {
+        out << separator << territory->territory_name;
+        separator = ", ";
+    }
+    out<<endl;
+    return out;
 }
 
-/*istream& operator>>(istream& istmr, const Player& p)
-{
-    return istmr  >> p.name;
-}*/
+// stream extraction operator
+istream& operator>>(istream &in, Player& p) {
+    cout << "Enter Player Name ";
+    in >> p.name;
+    return in;
+}
+
+vector <Territory*> Player::toDefend(){
+    cout << "Inside toDefend of Player" << endl;
+    Territory * tt1=new Territory("United Kingdome",0,5);
+    Territory * tt2=new Territory("Ireland",0,5);
+    vector<Territory*> t={tt1,tt2};
+    return t;
+}
+
+vector <Territory*> Player::toAttack(){
+    cout << "Inside toAttack of Player" << endl;
+    Territory * tt=new Territory("France",0,5);
+    vector<Territory*> t={tt};
+    return t;
+}
+
+void Player::issueOrder(){
+    cout << "Inside issueOrder of Player" << endl;
+    Order * order = new Order();
+    this->orders->list_orders.push_back(order);
+}
+
+OrdersList* Player::getOrders(){
+    return orders;
+}
 
--- Player.h
+++ Player.h
@@ -1,57 +1,77 @@
-//
-//  Player.hpp
-//  Comp 345 a1
-//
-//  Created by Beatrice Cobo on 2021-09-28.
-//
+/**
+ Part 2: Player.h
+ */
 
 #ifndef Player_h
 #define Player_h
 
-#include <list>
+#include <vector>
 #include <string>
 #include <iostream>
+#include <vector>
 using namespace std;
 
-class Territory{
-public:
-    Territory();
-    
+//Territory dummy class declaration
+class Territory
+{
+    public:
+        Territory();
+        Territory (string terr_name, int contin_ref, int num_of_armies);
+        Territory (const Territory &t1); //Copy constructor
+        Territory& operator= (const Territory& t1); // overloaded assignment operator
+        friend ostream & operator << (ostream &out, const Territory &t1);// stream insertion operator
+        friend istream & operator >> (istream &in,  Territory &t1);// stream extraction operator
+        string getName();
+        string territory_name;
+        int continent_ref;
+        int army_nb;
 };
 
+//Card dummy class declaration
 class Card{
     
 };
 
+//Order dummy class declaration
 class Order{
 public:
     Order();
+    string description;
     
 };
+
+//OrdersList dummy class declaration
+class OrdersList{
+public:
+    vector <Order*> list_orders;
+    
+};
+
+//Hand dummy class declaration
 class Hand{
     
 };
 
-/**
- 
- */
+//Player class declaration
 class Player{
 public:
     Player(); //default constructor
-    Player(string name, Hand * hand, list <Territory*> territories); // constructor
+    Player(string name, Hand * hand, vector <Territory*> territories); // constructor
     Player(const Player &p); //copy constructor
+    ~Player(); //destructor
     Player& operator =(const Player& p); // assignment operator
-    friend ostream& operator<<(ostream& ostmr, const Player& p); // stream insertion operator
-    //friend istream& operator<<(istream& istmr, const Player& p);
-    list <Territory*> toDefend();
-    list <Territory*> toAttack();
-    list <Order*> issueOrder();
+    friend ostream& operator << (ostream &out, const Player& p); // stream insertion operator
+    friend istream& operator >> (istream &in , Player& p); // stream extraction operator
+    vector <Territory*> toDefend();
+    vector <Territory*> toAttack();
+    void issueOrder();
+    OrdersList* getOrders();
     
 private:
     string name;
     Hand * hand;
-    list <Territory*> territories;
-    list <Order*> orders;
+    vector <Territory*> territories;
+    OrdersList* orders;
     
 };
 
