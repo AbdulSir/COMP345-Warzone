b6fd40dd73bc5edb947102188f8d8de83ec8e52f
wilson <wh.dev@icloud.com>
2021-11-29 20:08:02 -0500

    update
    Co-authored-by: bcobo341 <bcobo341@users.noreply.github.com>

--- PlayerStrategies.cpp
+++ PlayerStrategies.cpp
@@ -1,15 +1,146 @@
 #include "PlayerStrategies.h"
+#include "Orders.h"
+#include <iostream>
+#include <string>
+using namespace std;
 
 PlayerStrategy::PlayerStrategy(Player* p) {
     this->p = p;
 }
+PlayerStrategy::~PlayerStrategy(){
+    delete p;
+}
 
-Neutral::Neutral(Player* p): PlayerStrategy(p) {}
+PlayerStrategy::PlayerStrategy(const PlayerStrategy &p){
+    this->p = new Player(p);
+}
+
+Neutral::Neutral(Player* p): PlayerStrategy(p) {
+    isAttacked = false;
+}
+
+Neutral::Neutral(const Neutral &n):PlayerStrategy(n) {
+}
+
+void Neutral::issueOrder() {
+    if (isAttacked) {
+        Aggressive::issueOrder();
+    }
+}
+
+void Neutral::toDefend() {
+    this->isAttacked = true;
+    if (isAttacked) {
+        Aggressive::toDefend();
+    }
+}
+
+void Neutral::toAttack() {
+    if (isAttacked) {
+        Aggressive::toAttack();
+    }
+}
 
 Cheater::Cheater(Player* p): PlayerStrategy(p) {}
 
-Human::Human(Player* p): PlayerStrategy(p) {}
+Cheater::Cheater(const Cheater &c):PlayerStrategy(c) {
+}
 
-Agressive::Agressive(Player* p): PlayerStrategy(p) {}
+void Cheater::issueOrder() {
+
+}
+
+void Cheater::toDefend() {
+
+}
+
+void Cheater::toAttack() {
+
+}
+
+Human::Human(Player* p): PlayerStrategy(p) {}
+Human::Human(const Human &h):PlayerStrategy(h) {
+}
+
+void Human::issueOrder() {
+    cout << "Which order would you like to execute" << endl;
+    string command;
+    cin >> command;
+    switch(command) {
+        case "Deploy":
+            string territoryName;
+            int armyNumber;
+            cout << "Which territory would you like to deploy" << endl;
+            cin >> territoryName;
+            cout << "How many armies would you like to deploy";
+            cin >> armyNumber;
+            for (auto t: p->getTerritories()) {
+                if (t->getName() == territoryName) {
+                    p->getOrders()->addOrder(new Deploy(p, t, armyNumber))
+                }
+            }
+            break;
+        case "Advance":
+            break;
+        case "Airlift":
+            break;
+        case "Bomb":
+            break;
+        case "Blockade":
+            break;
+        case "Negotiate":
+            break;
+    }
+}
+
+void Human::toDefend() {
+
+}
+
+void Human::toAttack() {
+
+}
+
+Aggressive::Aggressive(Player* p): PlayerStrategy(p) {}
+Aggressive::Aggressive(const Aggressive &a):PlayerStrategy(a) {
+}
 
 Benevolent::Benevolent(Player* p): PlayerStrategy(p) {}
+Benevolent::Benevolent(const Benevolent &b):PlayerStrategy(b) {
+}
+
+ostream& operator<< (ostream &out, const PlayerStrategy& p) {
+    out << "Player strategy: default" << endl;
+    out << "Player: " << p.p->getName() << endl;
+    return out;
+}
+
+ostream& operator<< (ostream &out, const Neutral& p) {
+    out << "Player strategy: Neutral" << endl;
+    out << "Player: " << p.p->getName() << endl;
+    return out;
+}
+
+ostream& operator<< (ostream &out, const Cheater& p) {
+    out << "Player strategy: Cheater" << endl;
+    out << "Player: " << p.p->getName() << endl;
+    return out;
+}
+
+ostream& operator<< (ostream &out, const Human& p) {
+    out << "Player strategy: Human" << endl;
+    out << "Player: " << p.p->getName() << endl;
+    return out;
+}
+
+ostream& operator<< (ostream &out, const Aggressive& p) {
+    out << "Player strategy: Aggressive" << endl;
+    out << "Player: " << p.p->getName() << endl;
+    return out;
+}
+
+ostream& operator<< (ostream &out, const Benevolent& p) {
+    out << "Player strategy: Benevolent" << endl;
+    out << "Player: " << p.p->getName() << endl;
+    return out;
+}
--- PlayerStrategies.h
+++ PlayerStrategies.h
@@ -1,51 +1,72 @@
 #include 'Player.h'
-
+#include <iostream>
 class Player;
 class PlayerStrategy {
     public:
         Player* p;
         PlayerStrategy(Player* p);
+        ~PlayerStrategy();
+        PlayerStrategy(const PlayerStrategy &p); //copy constructor
         virtual void issueOrder() = 0;
         virtual void toAttack() = 0;
         virtual void toDefend() = 0;
+        PlayerStrategy& operator =(const PlayerStrategy& p); //assignment operator
+        friend ostream& operator << (ostream &out, const PlayerStrategy& p); // stream insertion operator
 };
 
 class Neutral: public PlayerStrategy {
     public:
+        bool isAttacked;
         Neutral(Player* p);
+        Neutral(const Neutral &n); //copy constructor
         void issueOrder();
         void toAttack();
         void toDefend();
+        Neutral& operator =(const Neutral& n); //assignment operator
+        friend ostream& operator << (ostream &out, const Neutral& n); // stream insertion operator
 };
 
 class Cheater: public PlayerStrategy {
     public:
         Cheater(Player* p);
+        Cheater(const Cheater &c); //copy constructor
         void issueOrder();
         void toAttack();
         void toDefend();
+        Cheater& operator =(const Cheater& c); //assignment operator
+        friend ostream& operator << (ostream &out, const Cheater& c); // stream insertion operator
 };
 
 class Human: public PlayerStrategy {
     public:
         Human(Player* p);
+        Human(const Human &h); //copy constructor
         void issueOrder();
         void toAttack();
         void toDefend();
+        Human& operator =(const Human& h); //assignment operator
+        friend ostream& operator << (ostream &out, const Human& h); // stream insertion operator
+        
 };
 
-class Agressive: public PlayerStrategy {
+class Aggressive: public PlayerStrategy {
     public:
-        Agressive(Player* p);
+        Aggressive(Player* p);
+        Aggressive(const Aggressive &a); //copy constructor
         void issueOrder();
         void toAttack();
         void toDefend();
+        Aggressive& operator =(const Aggressive& a); //assignment operator
+        friend ostream& operator << (ostream &out, const Aggressive& a); // stream insertion operator
 };
 
 class Benevolent: public PlayerStrategy {
     public:
         Benevolent(Player* p);
+        Benevolent(const Benevolent &b); //copy constructor
         void issueOrder();
         void toAttack();
         void toDefend();
+        Benevolent& operator =(const Benevolent& b); //assignment operator
+        friend ostream& operator << (ostream &out, const Benevolent& b); // stream insertion operator
 };
